d71f78097e15784a193fc2c499dbffc1b1d32f53
nacz0 <natmach2004@gmail.com>
2023-08-27 22:13:47 +0200

    uninstalling prisma and changing the code

--- socketQuiz/index.ts
+++ socketQuiz/index.ts
@@ -1,8 +1,5 @@
 const http = require("http").createServer();
 import type { Socket } from "socket.io";
-import { nanoid } from "nanoid";
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
 const io = require("socket.io")(http, {
   cors: { origin: "*" },
 });
@@ -10,34 +7,13 @@ const io = require("socket.io")(http, {
 io.on("connection", (socket: Socket) => {
   console.log("a user connected");
   socket.on("start_game", async (data) => {
-    const game = new Game(data.quizId);
-    game.id = nanoid();
-    game.data = await prisma.quiz
-      .findFirst({
-        where: { id: game.quizId },
-        include: {
-          questions: {
-            include: { answers: true },
-          },
-        },
-      })
-      .then(() => {
-        socket.join(game.id);
-        socket.emit("pin");
-        console.log(game.data);
-      });
+    const randomPIN = Math.floor(Math.random() * 1000000).toString();
+    socket.join(randomPIN);
+  });
+  socket.on("join_game", (pin) => {
+    //pin
+    socket.join(pin);
   });
 });
 
 http.listen(8000, () => console.log("Server is running..."));
-
-class Game {
-  quizId: string;
-  id!: string;
-  data: any;
-  pin: string;
-  constructor(quizId: string) {
-    this.quizId = quizId;
-    this.pin = Math.floor(Math.random() * 1000000).toString();
-  }
-}
