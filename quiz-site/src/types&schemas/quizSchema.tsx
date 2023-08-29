5b85ae84bbef713080ab85112109df02cb62021d
nacz0 <natmach2004@gmail.com>
2023-08-29 16:22:11 +0200

    changed quizSchema

--- quiz-site/src/types&schemas/quizSchema.tsx
+++ quiz-site/src/types&schemas/quizSchema.tsx
@@ -7,37 +7,36 @@ export const quizSchema = z.object({
     image: z.string().optional(),
   }),
   questions: z.array(
-    z
-      .object({
-        question: z.string(),
-        ytLink: z.string().optional(),
-        image: z.string().optional(),
-        answers: z
-          .array(
-            z
-              .object({
-                text: z.string(),
-                isCorrect: z.boolean(),
-                image: z.string(),
-              })
-              .partial()
-              .refine(
+    z.object({
+      text: z.string(),
+      ytLink: z.string().optional(),
+      image: z.string().optional(),
+      answers: z
+        .array(
+          z
+            .object({
+              text: z.string(),
+              isCorrect: z.boolean(),
+              image: z.string(),
+            })
+            .partial()
+          /*.refine(
                 ({ text, image }) => {
-                  text !== "" || image !== "";
+                  text !== ""; //|| image !== "";
                 },
                 { message: "Answer must have text or image" }
-              )
-          )
+              )*/
+        )
 
-          .min(4)
-          .max(4),
-      })
-      .refine(
+        .min(4)
+        .max(4),
+    })
+    /*.refine(
         ({ ytLink, image }) => {
           ytLink !== "" && image !== "";
         },
         { message: "Question must have either a youtube link or an image" }
-      )
+      )*/
   ),
 });
 
