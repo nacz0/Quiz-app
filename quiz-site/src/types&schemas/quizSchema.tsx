1e10985790509afce84e330c091e649c669e52f2
nacz0 <natmach2004@gmail.com>
2023-08-30 15:41:14 +0200

    quiz schema changed

--- quiz-site/src/types&schemas/quizSchema.tsx
+++ quiz-site/src/types&schemas/quizSchema.tsx
@@ -1,43 +1,83 @@
 import { z } from "zod";
 
+const questionSchema = z
+  .object({
+    text: z.string(),
+    ytLink: z.string().optional(),
+    image: z.string().optional(),
+    answerTime: z.number(),
+    type: z.enum(["answers", "true_false", "input", "slider"]),
+    answers: z
+      .array(
+        z
+          .object({
+            text: z.string(),
+            isCorrect: z.boolean(),
+            image: z.string(),
+          })
+          .partial()
+          .superRefine(({ text, image }, ctx) => {
+            if (
+              text !== undefined &&
+              text !== "" &&
+              image !== undefined &&
+              image !== ""
+            ) {
+              ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: "Answer must not have an image and text",
+                path: ["text", "image"],
+              });
+            }
+            if (
+              (text === undefined || text === "") &&
+              (image === undefined || image === "")
+            ) {
+              ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: "Answer must have an image or some text",
+                path: ["text", "image"],
+              });
+            }
+          })
+      )
+      .min(4)
+      .max(4)
+      .superRefine((answers, ctx) => {
+        if (answers.find((answer) => answer.isCorrect) === undefined) {
+          console.log("must have correct answer");
+          console.log("no correct answer");
+          ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: "Question must have a correct answer",
+            path: ["answers"],
+          });
+        }
+      }),
+  })
+  .superRefine(({ image, ytLink }, ctx) => {
+    if (
+      image !== undefined &&
+      image !== "" &&
+      ytLink !== undefined &&
+      ytLink !== ""
+    ) {
+      console.log("must not have image and yt link");
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Question must not have an image and a yt link",
+        path: ["image", "ytLink"],
+      });
+    }
+  });
+
 export const quizSchema = z.object({
   quiz: z.object({
     title: z.string().min(1, { message: "Title is required" }),
     description: z.string().optional(),
     image: z.string().optional(),
   }),
-  questions: z.array(
-    z.object({
-      text: z.string(),
-      ytLink: z.string().optional(),
-      image: z.string().optional(),
-      answers: z
-        .array(
-          z
-            .object({
-              text: z.string(),
-              isCorrect: z.boolean(),
-              image: z.string(),
-            })
-            .partial()
-          /*.refine(
-                ({ text, image }) => {
-                  text !== ""; //|| image !== "";
-                },
-                { message: "Answer must have text or image" }
-              )*/
-        )
-
-        .min(4)
-        .max(4),
-    })
-    /*.refine(
-        ({ ytLink, image }) => {
-          ytLink !== "" && image !== "";
-        },
-        { message: "Question must have either a youtube link or an image" }
-      )*/
-  ),
+  questions: z.array(questionSchema),
 });
 
 export type QuizFormValues = z.infer<typeof quizSchema>;
