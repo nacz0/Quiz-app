925460bbf5800132e684f5d0556d33cabf013586
nacz0 <natmach2004@gmail.com>
2023-08-30 19:48:57 +0200

    bg to show when a quiz does not have an image

---
+++ quiz-site/src/svg/bg.tsx
@@ -0,0 +1,38 @@
+export function BgIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      id="visual"
+      viewBox="0 0 900 900"
+      width="900"
+      height="900"
+      version="1.1"
+    >
+      <rect x="0" y="0" width="900" height="900" fill="#fbbf24" />
+      <defs>
+        <linearGradient id="grad1_0" x1="0%" y1="0%" x2="100%" y2="100%">
+          <stop offset="30%" stopColor="#fbbf24" stopOpacity="1" />
+          <stop offset="70%" stopColor="#fbbf24" stopOpacity="1" />
+        </linearGradient>
+      </defs>
+      <defs>
+        <linearGradient id="grad2_0" x1="0%" y1="0%" x2="100%" y2="100%">
+          <stop offset="30%" stopColor="#fbbf24" stopOpacity="1" />
+          <stop offset="70%" stopColor="#fbbf24" stopOpacity="1" />
+        </linearGradient>
+      </defs>
+      <g transform="translate(900, 0)">
+        <path
+          d="M0 572.8C-66 535.1 -132 497.5 -192.9 465.6C-253.8 433.7 -309.6 407.6 -369.8 369.8C-430 332.1 -494.6 282.7 -529.2 219.2C-563.7 155.6 -568.2 77.8 -572.8 0L0 0Z"
+          fill="#2dd4bf"
+        />
+      </g>
+      <g transform="translate(0, 900)">
+        <path
+          d="M0 -572.8C68.8 -549.3 137.5 -525.9 207.8 -501.7C278.1 -477.4 349.9 -452.3 405 -405C460.1 -357.7 498.7 -288.2 523.8 -217C549 -145.8 560.9 -72.9 572.8 0L0 0Z"
+          fill="#2dd4bf"
+        />
+      </g>
+    </svg>
+  );
+}
