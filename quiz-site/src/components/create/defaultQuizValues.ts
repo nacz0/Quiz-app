56a2a49f94ef61f076544c9b255de787bcbe44d7
nacz0 <natmach2004@gmail.com>
2023-09-03 02:27:49 +0200

    default quiz values

---
+++ quiz-site/src/components/create/defaultQuizValues.ts
@@ -0,0 +1,45 @@
+enum QuestionType {
+  ANSWERS = "answers",
+  TRUE_FALSE = "true_false",
+  INPUT = "input",
+  SLIDER = "slider",
+}
+
+export const defaultQuizValues = {
+  quiz: {
+    title: "",
+    description: "",
+    image: "",
+  },
+  questions: [
+    {
+      text: "",
+      ytLink: "",
+      image: "",
+      answerTime: 20,
+      type: QuestionType.ANSWERS,
+      answers: [
+        {
+          text: "",
+          isCorrect: false,
+          image: "",
+        },
+        {
+          text: "",
+          isCorrect: false,
+          image: "",
+        },
+        {
+          text: "",
+          isCorrect: false,
+          image: "",
+        },
+        {
+          text: "",
+          isCorrect: false,
+          image: "",
+        },
+      ],
+    },
+  ],
+};
