7720999d621ed74fd28a58a0d9c6c96a169bd62e
nacz0 <natmach2004@gmail.com>
2023-09-08 17:55:22 +0200

    single answer

--- quiz-site/src/components/create/SingleAnswer.tsx
+++ quiz-site/src/components/create/SingleAnswer.tsx
@@ -1,9 +1,14 @@
+import { useEffect } from "react";
 import {
   Control,
+  UseFormTrigger,
+  useWatch,
   type UseFormRegister,
   type UseFormSetValue,
 } from "react-hook-form";
+import { quizMaxLength } from "~/types&schemas/quizMaxLength";
 import type { savedDraftQuizValues } from "~/types&schemas/savedDraftSchema";
+import { MediaInput } from "./MediaInput";
 import { ResizableInput } from "./resizableInput";
 
 export function SingleAnswer(props: {
@@ -11,9 +16,26 @@ export function SingleAnswer(props: {
   register: UseFormRegister<savedDraftQuizValues>;
   setValue: UseFormSetValue<savedDraftQuizValues>;
   control: Control<savedDraftQuizValues>;
+  trigger: UseFormTrigger<savedDraftQuizValues>;
 }) {
-  const { currentQuestion, register, setValue, control } = props;
+  const { currentQuestion, register, setValue, control, trigger } = props;
+  const question = useWatch({
+    control,
+    name: `questions.${currentQuestion}`,
+  });
+
+  useEffect(() => {
+    void trigger(`questions.${currentQuestion}`);
+  }, [currentQuestion, question, trigger]);
+
   const numOfAnswers = [0, 1, 2, 3];
+  const colors = ["bg-red-600", "bg-green-600", "bg-blue-600", "bg-yellow-500"];
+  const border = [
+    "border-red-700",
+    "border-green-700",
+    "border-blue-700",
+    "border-yellow-600",
+  ];
 
   return (
     <div className="flex h-full w-full flex-col items-center ">
@@ -22,24 +44,42 @@ export function SingleAnswer(props: {
         currentQuestion={currentQuestion}
         register={register}
       />
-      <div className="flex flex-col gap-4 bg-red-400 p-4">
+      <MediaInput
+        control={control}
+        setValue={setValue}
+        currentQuestion={currentQuestion}
+        register={register}
+      />
+      <div className=" mt-5 grid grid-cols-2 gap-2">
         {numOfAnswers.map((answer) => (
-          <div className="flex flex-row gap-3" key={answer}>
-            <input
+          <div
+            className={`${colors[answer]} ${border[answer]} relative flex h-24 w-40 flex-row rounded-lg border-b-4 p-2 `}
+            key={answer}
+          >
+            <textarea
               defaultValue={""}
+              spellCheck={false}
+              maxLength={quizMaxLength.answer.text}
               {...register(
                 `questions.${currentQuestion}.answers.${answer}.text`
               )}
+              placeholder={`Dodaj odpowiedź ${answer + 1}. `}
               key={`questions.${currentQuestion}.answers.${answer}.text`}
-            ></input>
-
-            <input
-              type="checkbox"
-              {...register(
-                `questions.${currentQuestion}.answers.${answer}.isCorrect`
-              )}
-              key={`questions.${currentQuestion}.answers.${answer}.isCorrect`}
-            ></input>
+              className="mt-2 h-full w-full   resize-none bg-transparent text-xs font-semibold text-white outline-none placeholder:text-white "
+            ></textarea>
+            <div className="absolute right-0 top-0">
+              <input
+                type="checkbox"
+                className="relative h-4 w-4 appearance-none rounded-full border-2 border-white  bg-transparent outline-none  transition-colors 
+                duration-300 before:absolute before:left-[1.5px] before:top-[1.5px] 
+                before:w-[9px] before:bg-white before:content-[''] before:[clip-path:polygon(14%_44%,_0_65%,_50%_100%,_100%_16%,_80%_0%,_43%_62%)]
+                 checked:bg-green-400  checked:before:h-[9px]"
+                {...register(
+                  `questions.${currentQuestion}.answers.${answer}.isCorrect`
+                )}
+                key={`questions.${currentQuestion}.answers.${answer}.isCorrect`}
+              ></input>
+            </div>
           </div>
         ))}
       </div>
