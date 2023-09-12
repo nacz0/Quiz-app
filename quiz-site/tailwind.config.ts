e17061e78e33c333be4c047cdd1dbd8e39f25783
nacz0 <natmach2004@gmail.com>
2023-09-12 23:11:21 +0200

    tailwind config extend

--- quiz-site/tailwind.config.ts
+++ quiz-site/tailwind.config.ts
@@ -3,7 +3,30 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        leftToRight: {
+          "0%": { backgroundPosition: "left" },
+          "100%": { backgroundPosition: "right" },
+        },
+        gradientSlide: {
+          "0%": {
+            borderImageSlice: "1",
+            borderImageSource: "linear-gradient(0deg,red,blue)",
+            color: "red",
+          },
+          "100%": {
+            borderImageSlice: "1",
+            borderImageSource: "linear-gradient(360deg,red,blue)",
+            color: "blue",
+          },
+        },
+      },
+      animation: {
+        leftToRight: "leftToRight 3s infinite alternate",
+        gradientSlide: "gradientSlide 3s infinite ",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
