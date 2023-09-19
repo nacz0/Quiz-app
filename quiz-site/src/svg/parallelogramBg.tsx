fd77bd9fc2b20d1dbdf1d90756273bb2966b280f
nacz0 <natmach2004@gmail.com>
2023-09-19 19:56:43 +0200

    svgs

---
+++ quiz-site/src/svg/parallelogramBg.tsx
@@ -0,0 +1,11 @@
+export function ParallelogramBg(props: { height: number }) {
+  return (
+    <svg
+      viewBox="0 199.5 600 100.5"
+      width={props.height * 5.97}
+      height={props.height}
+    >
+      <path d="M 600 200 L 500 300 L -0.5 299.494 L 100 199.398 L 600 200 Z" />
+    </svg>
+  );
+}
